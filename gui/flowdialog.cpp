bb24de06665bb7bc56523ea0237b1153265c5ffd
BlabeZ <zhengweifeng051922@163.com>
2026-06-19 20:34:22 +0800

    调整: 金额输入框默认空白显示(特殊值文字)

--- gui/flowdialog.cpp
+++ gui/flowdialog.cpp
@@ -108,11 +108,12 @@ void FlowDialog::setupUI()
     connect(m_radioExpense, &QRadioButton::toggled, this, &FlowDialog::onTypeChanged);
     connect(m_radioIncome,  &QRadioButton::toggled, this, &FlowDialog::onTypeChanged);
 
-    // 金额字段
+    // 金额字段（0.00时显示为空白）
     m_amountSpin = new QDoubleSpinBox;
     m_amountSpin->setRange(0.00, 99999999.99);
     m_amountSpin->setDecimals(2);
-    m_amountSpin->setPrefix(" ");
+    m_amountSpin->setPrefix("¥ ");
+    m_amountSpin->setSpecialValueText(" ");
     m_amountSpin->setValue(0.00);
     m_amountSpin->setMinimumWidth(200);
     form->addRow("金额:", m_amountSpin);
